d94e5ca5331b38cb478ba14ab2d2cd4fc669cbf5
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-07-28 20:22:22 -0600

    add fish types and qty

--- hash_1.rb
+++ hash_1.rb
@@ -2,4 +2,6 @@ ocean = {
   "name" => "Carribean",
   "reef_type" => "Coral",
   "water_color" => "Aqua",
+  "fish_qty" => 5,
+  "fish_types" => ["Angel", "Surgeon", "Clown", "Butterfly", "Yellow Tang"]
 }
